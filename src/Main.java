1f553716b15bae8a0633518f57cc8a5be9547a01
Wiktor <notnow>
2024-03-15 00:05:02 +0100

    Code update.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import PasswordGenerator.*;
 public class Main {
     public static void main(String[] args) {
-
         PasswordGenerator password = new PasswordGenerator();
         String createdPassword = password.generate();
         System.out.println(createdPassword);
-
     }
 }
